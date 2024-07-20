90637af2e2f9b262adacbaf6b6dbe24b495602bf
RicardoCuadros <ricardo22042003@gmail.com>
2024-07-20 06:01:58 -0500

    cambio de url para imagen banner

--- src/customer/components/HomeCarousel/MainCarouselData.js
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -1,14 +1,14 @@
 export const mainCarouselData=[
     {
-        image: "https://scontent.flim39-1.fna.fbcdn.net/v/t39.30808-6/448943075_122159102150161737_2400020431040221248_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=XO3Ll8cJvhMQ7kNvgFd3efX&_nc_ht=scontent.flim39-1.fna&oh=00_AYBA48ahLOHm4mxSOjmrT8M7t2sX0W6A5m4BJ8mXcTuJKA&oe=667CC99A",
-        path:"/women/clothing/lengha_choli"
+        image: "https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/448943075_122159102150161737_2400020431040221248_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFR36pc58JxTuAoMuYPobMFUS-h37C6ORNRL6HfsLo5E_WfCoF9cIvsN3BvoBtX45eHw3E6CJhuv0ggQEmXYwya&_nc_ohc=E0YfchfhkcQQ7kNvgFYyBuP&_nc_ht=scontent-lim1-1.xx&oh=00_AYAuGVk6_9CMUwq7sSQCoCI3lOOZq5e4lnlyh7f9q6VMcQ&oe=66A10ADA",
+        path:""
     },
     {
-        image: "https://scontent.flim39-1.fna.fbcdn.net/v/t39.30808-6/448884499_122159103242161737_3179066761787329481_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=DISjH8OZNVwQ7kNvgHhnQ6P&_nc_ht=scontent.flim39-1.fna&oh=00_AYAHymjKqKtSVYa6pg9Zy-PzMMGWOhB3Ba6P7RsDz1arrg&oe=667CCD69",
-        path:"/women/clothing/lengha_choli"
+        image: "https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/448884499_122159103242161737_3179066761787329481_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGsvZqsv-ldUjpd60pXmjmv8QTTWHEINC3xBNNYcQg0LcbtSP8374Ocd1t7zjnqxKuk5l-5BIbNQFFRT5DiRMZ6&_nc_ohc=XUYribWeTSAQ7kNvgFmw53Q&_nc_ht=scontent-lim1-1.xx&oh=00_AYDTHg06j7D-YtIyBqmiYz3UQPbss8waMk7Rm0z5olD6uw&oe=66A10EA9",
+        path:""
     },
     {
-        image: "https://scontent.flim39-1.fna.fbcdn.net/v/t39.30808-6/448884119_122159103626161737_8819294700691733669_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=TQFnG5JNo8AQ7kNvgEbCqus&_nc_ht=scontent.flim39-1.fna&oh=00_AYDZ5piZPX_GO9Drb2kiQZoF0pgKBv95fkiGXrjDYI4shQ&oe=667CDCA2",
-        path:"/women/clothing/lengha_choli"
+        image: "https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/448884119_122159103626161737_8819294700691733669_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEbpFhxkSAMb1LG_19eyM5sD2FCdSSjINsPYUJ1JKMg27JF07o3XzMx2iFD3L7KpenqbILPPxA5kwQWW54Kunce&_nc_ohc=pkpKA9g8yuAQ7kNvgFvRaaI&_nc_ht=scontent-lim1-1.xx&oh=00_AYAA1trMa7-A0OPWqL0GK9f9uWHfPN1kLurFMx2ASf4oxQ&oe=66A11DE2",
+        path:""
     }
 ]
